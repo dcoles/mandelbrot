50f0e5e7d58cd2636bc8c7a274fe19b65609415c
David Coles <coles.david@gmail.com>
2020-07-19 18:12:05 -0700

    Render fractal in a Web Worker
    This prevents us blocking the main JS thread.

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,14 +1,13 @@
 /**
  * Draw Mandelbrot onto Canvas.
  *
- * @param canvas: HTML `canvas` to draw onto.
+ * @param {number} width Width of the drawing.
+ * @param {number} height Height of the drawing.
  */
-function drawMandelbrot(canvas) {
-    const ctx = canvas.getContext('2d');
-    let img = ctx.createImageData(canvas.width, canvas.height);
-
-    const xoff = -2 * img.width / 3;
-    const yoff = -img.height / 2;
+function drawMandelbrot(width, height) {
+    const bpp = 4; // bytes-per-pixel
+    const xoff = -2 * width / 3;
+    const yoff = -height / 2;
     const scale = 0.4;
     const maxIterations = 1000;
     const target = 20;  // Steps of target colour
@@ -16,42 +15,42 @@ function drawMandelbrot(canvas) {
 
     const growth = Math.pow(255, 1 / steps);
 
-    for (let i=0; i<img.data.length; i+=4) {
-        const x = Math.floor(i / 4) % img.width;
-        const y = Math.floor(i / (img.width * 4));
+    let data = new Uint8ClampedArray(width * height * bpp);
+    for (let i = 0; i < data.length; i += bpp) {
+        const x = Math.floor(i / bpp) % width;
+        const y = Math.floor(i / (width * bpp));
 
         // Adjusted for screen space
-        const xadj = (x + xoff) / img.height / scale;
-        const yadj = (y + yoff) / img.height / scale;
+        const xadj = (x + xoff) / height / scale;
+        const yadj = (y + yoff) / height / scale;
         const c = [xadj, yadj];
 
         let n = escape(c, maxIterations);
         if (n === Infinity) {
             // White (#ffffff)
-            img.data[i] = 0xff;  // R
-            img.data[i + 1] = 0xff;  // G
-            img.data[i + 2] = 0xff;  // B
-            img.data[i + 3] = 0xff;  // A
-            /*
-                    // For debugging
-                    } else if (n === target) {
-                        // Red (#ff0000)
-                        img.data[i] = 0xff;  // R
-                        img.data[i + 1] = 0x00;  // G
-                        img.data[i + 2] = 0x00;  // B
-                        img.data[i + 3] = 0xff;  //
-             */
+            data[i] = 0xff;  // R
+            data[i + 1] = 0xff;  // G
+            data[i + 2] = 0xff;  // B
+            data[i + 3] = 0xff;  // A
+        /* For debugging
+        } else if (n === target) {
+            // Red (#ff0000)
+            data[i] = 0xff;  // R
+            data[i + 1] = 0x00;  // G
+            data[i + 2] = 0x00;  // B
+            data[i + 3] = 0xff;  //
+        */
         } else {
             // Blue fire
             const intensity = Math.pow(growth, n - target);
-            img.data[i]   = (intensity - 2) * 0xff;  // R
-            img.data[i+1] = (intensity - 1) * 0xff;  // G
-            img.data[i+2] = intensity * 0xff;  // B
-            img.data[i+3] = 0xff;  // A
+            data[i]   = (intensity - 2) * 0xff;  // R
+            data[i+1] = (intensity - 1) * 0xff;  // G
+            data[i+2] = intensity * 0xff;  // B
+            data[i+3] = 0xff;  // A
         }
     }
 
-    ctx.putImageData(img, 0, 0);
+    return new ImageData(data, width, height);
 }
 
 
@@ -89,4 +88,22 @@ function f(z, c) {
         z[0] * z[0] - z[1] * z[1] + c[0],  // Real
         2 * z[0] * z[1] + c[1]  // Imaginary
     ]
+}
+
+/**
+ * WebWorker hook to render a new Mandelbrot fractal.
+ *
+ * Expected message: `[width, height]`
+ *
+ * @param e WebWorker message.
+ */
+onmessage = function(e) {
+    const size = e.data;
+    console.log('onmessage: ' + size);
+
+    const data = drawMandelbrot(size[0], size[1]);
+    console.log('Done');
+
+    // Pass buffer back to page
+    postMessage(data);
 }
